4ac958998add51c3d337d103c33fd17a96ddd096
AamilKalra <63241401+AamilKalra@users.noreply.github.com>
2020-06-10 19:03:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,32 +3,35 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint =Matter.Constraint;
 
+var ceiling_options, ceiling;
+var ball_options, ball;
+var chain, options;
 
 function setup() {
   var canvas = createCanvas(800,800);
   engine = Engine.create();
   world = engine.world;
 
-  var ceiling_options={
+   ceiling_options={
     isStatic:true
   }
 
-  var ceiling=Bodies.rectangle(400, 100, 200, 15,ceiling_options);
+   ceiling=Bodies.rectangle(400, 100, 200, 15,ceiling_options);
   World.add(world,ceiling)
 
-  var ball_options={
+   ball_options={
     isStatic:true
   }
-  var ball = Bodies.circle(400,300,70,ball_options);
+   ball = Bodies.circle(400,300,70,ball_options);
   World.add(world,ball)
   
-  var options={
+   options={
     bodyA:ball,
     bodyB:ceiling,
     length:100,
     stiffness:0.04
   }
-  var chain = Constraint.create(options);
+  chain = Constraint.create(options);
   World.add(world,chain);
   
 }
@@ -40,7 +43,7 @@ function draw() {
 
   ellipse(ball.position.x,ball.position.y,70)
 
-  line(ball.position.x,ball.position.y,ceiling.x,ceiling,y)
+  line(ball.position.x,ball.position.y,ceiling.x,ceiling.y)
   
   
 }
